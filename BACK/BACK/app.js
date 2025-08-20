0be92269a9d118fe1051f5206c5973692b56f472
joker2916 <yannmagadju30@gmail.com>
2025-08-20 01:44:09 +0100

    document a reviser

--- BACK/BACK/app.js
+++ BACK/BACK/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Backend Render OK");
+});
+
 app.use("/api/sensors", require("./routes/Sensor"));
 app.use("/api/actuators", require("./routes/Actuator"));
 app.use("/api/alerts", require("./routes/Alert"));
@@ -15,7 +19,7 @@ app.use("/api/users", require("./routes/User"));
 /* Esp32 */
 app.use("/api/esp32", require("./routes/Esp32"));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
     console.log(`Serveur démarré sur le port ${PORT}`)
     //await initFirestore();
